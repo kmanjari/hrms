9857bb941fe840daeec06c202fd3f2c72ec26b75
KhuongDV <Khuong.dv@neo-lap.vn>
2017-12-08 14:34:07 +0700

    done 5.3 to 5.5

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,4 +28,15 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
     }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+        require base_path('routes/console.php');
+    }
 }
